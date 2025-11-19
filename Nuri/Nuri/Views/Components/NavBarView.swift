40e73c36dafd953f8a6c14e46958c646fa8a761c
Sama Ahmed <26samahmed@csu.fullerton.edu>
2025-11-18 19:14:02 -0800

    start adding the navbar

---
+++ Nuri/Nuri/Views/Components/NavBarView.swift
@@ -0,0 +1,24 @@
+//
+//  NavBarView.swift
+//  Nuri
+//
+//  Created by sama saad on 11/18/25.
+//
+
+import SwiftUI
+
+struct NavBarView: View {
+    
+    var body: some View {
+        ZStack {
+            Color(red: 245/255, green: 245/255, blue: 245/255)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+    
+    
+}
+
+#Preview {
+    NavBarView()
+}
