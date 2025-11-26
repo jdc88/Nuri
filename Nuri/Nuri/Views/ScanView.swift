fbfc14e4efd1fa926ac761a4fb39d45eab4be2dd
Sama Ahmed <26samahmed@csu.fullerton.edu>
2025-11-25 16:19:39 -0800

    Add Top Bar & NavBar to all pages

---
+++ Nuri/Nuri/Views/ScanView.swift
@@ -0,0 +1,35 @@
+//
+//  ScanView.swift
+//  Nuri
+//
+//  Created by sama saad on 11/25/25.
+//
+
+import SwiftUI
+
+struct ScanView: View {
+    
+    var body: some View {
+        ZStack {
+            Color(red: 245/255, green: 245/255, blue: 245/255)
+                .ignoresSafeArea()
+
+            VStack(spacing: 0) {
+                
+                TopBarView()
+                ScrollView {
+                    VStack(spacing: 30) {
+
+                    }
+                    .padding(.top, 20) // Space below top bar
+                }
+                NavBarView()
+                    .ignoresSafeArea(edges: .bottom)
+            }
+        }
+    }
+}
+
+#Preview {
+    ScanView()
+}
